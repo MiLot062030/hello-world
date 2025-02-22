e7d059cca72d587c58e2327e4b92d2e7159c99b2
MiLot062030 <mrdgallenero@mymail.mapua.edu.ph>
2025-02-22 22:39:29 +0800

    Add files via upload

---
+++ def determine_​grade(score)​.py
@@ -0,0 +1,54 @@
+def determine_​grade(score):​ 
+
+    ​if 90 <= score <= 100:​ 
+
+        ​return 'A'​ 
+
+    ​elif 80 <= score < 90:​ 
+
+        ​return 'B'​ 
+
+    ​elif 70 <= score < 80:​ 
+
+        ​return 'C'​ 
+
+    ​elif 60 <= score < 70:​ 
+
+        ​return 'D'​ 
+
+    else: 
+
+        ​return 'F'​ 
+
+  
+
+​​def main():​ 
+
+    try: 
+
+        ​score = float(input​("Please enter your exam score (out of ​100): "))​ 
+
+        
+
+        ​if score​ < ​0 or score > 100:​ 
+   ​print("Invalid score. Please enter a​ score ​between 0 and 100​.") 
+
+            return 
+
+  
+
+        grade = determine_grade(score) 
+
+        print(f"Your grade is: {​grade}")​ 
+
+     
+
+    except ValueError: 
+
+        ​print("Invalid input. Please enter a​ numeric value.") 
+
+  
+
+if __name__ == "__main__": 
+
+    main() 
